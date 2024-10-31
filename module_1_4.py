348adedf92611b852dd407249d61f1b96eeb4d17
HasaGI72 <evgeniababaeva766@gmail.com>
2024-10-31 18:16:41 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,17 @@
+#1
+my_string = input("Здравствуйте препод как ваше имя? " )
+#2
+print(len(my_string))
+#3
+print("хорощего дня/вечера вам". upper())
+#3
+print("хорощего дня/вечера вам". lower())
+#3
+print("хорощего дня/вечера вам". replace(" ","-"))
+#4
+first_character = my_string[0]
+print( first_character[0])
+#5
+first_character = my_string[-1]
+print( first_character[-1])
+
